d1815ae433e143b404b2fe3722581390dcb3bfba
DanOvando <danovan@uw.edu>
2020-08-13 21:33:32 -0700

    aggregation works

--- server.R
+++ server.R
@@ -31,10 +31,78 @@ server <- function(input, output, session) {
     )
   })
 
+# filter and or subset data
 
+
+# plot data
 
   output$inspectplot <-
     renderPlot(inspect_plot(lcomps, x = input$inspectplot_x, y = input$inspectplot_y))
+
+  # aggregate length data
+
+  output$select_ldata <- renderUI({
+    vars <- c(NA,colnames(lcomps[map_lgl(lcomps, is.numeric)]))
+    selectizeInput(
+      "select_ldata",
+      "Select the column with length data in it",
+      vars
+    )
+  })
+
+  output$select_tally <- renderUI({
+    vars <- c(NA,colnames(lcomps[map_lgl(lcomps, is.numeric)]))
+    selectizeInput(
+      "select_tally",
+      "Select the column with the numbers per length bin in it",
+      vars
+    )
+  })
+
+  output$select_groupers <- renderUI({
+    vars <- c("Choose Grouping Variables" = "",colnames(lcomps))
+    selectInput(
+      "select_groupers",
+      "Select the variables to group by",
+      vars,
+      multiple = TRUE
+    )
+  })
+
+  counter <- function(data, group_var = NULL, length_col,n_col = NA, type = "obs") {
+
+    group_var = ifelse(is.na(group_var), NULL, group_var)
+
+    group_var <- c(group_var, length_col)
+
+    if (type == "obs") {
+
+      out <- data %>%
+        group_by(across({{ group_var }})) %>%
+        count()
+    } else if (type == "counts"){
+
+      out <- data %>%
+        dplyr::group_by(dplyr::across({{ group_var }})) %>%
+        dplyr::summarise(n = sum(.data[[n_col]]))
+
+    }
+
+    return(ungroup(out))
+
+
+  }
+
+
+  grouped_lcomps <-  observeEvent(input$group,{
+
+    grouped_lcomps <- counter(lcomps, group_var = input$select_groupers,length_col = input$select_ldata)
+
+    output$grouped_lcomps <- renderDataTable(grouped_lcomps,  options = list(pageLength = 5))
+
+  })
+
+
 
 
 }
--- ui.R
+++ ui.R
@@ -8,6 +8,8 @@ library(janitor)
 library(readr)
 library(ggplot2)
 library(purrr)
+library(dplyr)
+library(tidyr)
 
 functions <- list.files(here::here("R"))
 
@@ -35,18 +37,47 @@ function(input,output){
     tabItems(
     tabItem(tabName = "description",
     h1("fishualizeR"),
-    "fishualizeR helps users visualize common fishery related data"),
+    "fishualizeR helps users visualize common fishery related data. To start, click on the 'Length Composition Data' tab."),
     tabItem(tabName = "lcomps",
       fluidRow(box(title = "Load Data", "This will be a button for loading your data and specifying details about your data", width = 12)),
-    h2("This is some text"),
-    "Followed by more text",
-    box(title = "Length Composition Data", dataTableOutput("lcomps"), width = 12),
+    fluidRow(box(title = "Length Composition Data", dataTableOutput("lcomps"), width = 12, solidHeader = TRUE, collapsible = TRUE)),
     fluidRow(
+    tabBox(
+    title = "Examine Raw Data",
+    id = "inspect",
+    tabPanel(
+    "Plot Raw Data",
     box(uiOutput("inspectplot_x")),
     box(uiOutput("inspectplot_y")),
-    plotOutput("inspectplot")
-    ))
-  ) # close dashboard body
+    plotOutput("inspectplot",
+               height = "300px"),
+    br(),
+    br()
+    ),
+    br(),
+    br(),
+    br(),
+    br(),
+    tabPanel(title = "Modify Raw Data",{
+      "Placeholder for some minimal data filtering"
+    }
+    ),
+    width = 12)
+    ), # close inspect fluidrow
+    fluidRow(
+      box(title = "Aggregate Lenth Data", width = 12,solidHeader = TRUE, collapsible = TRUE,
+          uiOutput("select_ldata")
+          ,
+      "If rows represent tallies per length bin, select column with number of length bin",
+      uiOutput("select_tally"),
+      uiOutput("select_groupers"),
+      actionButton("group","Aggregate Data"),
+      dataTableOutput("grouped_lcomps")
+
+      )
+    )
+    ) # close lcomps tab
+  )
 )
   dashboardPage(
     header,
